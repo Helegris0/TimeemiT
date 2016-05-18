68174a837bb35d950a553e23338658d85366d858
Nerg4l <gorog_laci@hotmail.com>
2016-05-18 21:08:19 +0200

    First version of client for workers

---
+++ WorkerClient/WorkerClient/MainWindow.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using UserService;
+using Xceed.Wpf.Toolkit;
+
+namespace WorkerClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ServiceConsumer serviceConsumer;
+        private User user;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            serviceConsumer = new ServiceConsumer();
+
+            foreach (TabItem item in tabControl.Items)
+            {
+                item.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void loginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string username = usernameTextBox.Text;
+            string password = passwordBox.Password;
+
+            user = serviceConsumer.Login(username, password);
+
+            if (user != null)
+            {
+                usernameTextBox.Clear();
+                passwordBox.Clear();
+
+                FillListBox();
+
+                StartInputIsEnabled(true);
+
+                userTab.IsSelected = true;
+            }
+        }
+
+        private void FillListBox()
+        {
+            listBox.Items.Clear();
+
+            Dictionary<DateTime, TimeSpan> dic = serviceConsumer.GetTimes(user);
+            foreach (var item in dic)
+            {
+                listBox.Items.Add(item.Key + " " + item.Value);
+            }
+        }
+
+        private void logoutButton_Click(object sender, RoutedEventArgs e)
+        {
+            loginTab.IsSelected = true;
+        }
+
+        private void registertStartButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (startDateTimePicker.Value != null)
+            {
+                serviceConsumer.StartWork(user, (DateTime)startDateTimePicker.Value);
+
+                StartInputIsEnabled(false);
+            }
+        }
+
+        private void registerEndButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (endDateTimePicker.Value != null)
+            {
+                serviceConsumer.StartWork(user, (DateTime)endDateTimePicker.Value);
+
+                StartInputIsEnabled(true);
+            }
+
+            startDateTimePicker.Text = "";
+            endDateTimePicker.Text = "";
+
+            FillListBox();
+        }
+
+        private void StartInputIsEnabled(bool value)
+        {
+            startDateTimePicker.IsEnabled = value;
+            registertStartButton.IsEnabled = value;
+
+            endDateTimePicker.IsEnabled = !value;
+            registerEndButton.IsEnabled = !value;
+        }
+    }
+}
---
+++ WorkerClient/WorkerClient/ServiceConsumer.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using UserService;
+
+namespace WorkerClient
+{
+    class ServiceConsumer
+    {
+
+        private string loginWebServiceName = "http://localhost:55374/LoginService.svc";
+        private string submitTimeWebServiceName = "http://localhost:55374/SubmitTimeService.svc";
+        private string checkTimeWebServiceName = "http://localhost:55374/CheckTimeService.svc";
+        private string getUsersWebServiceName = "http://localhost:55374/GetUsersService.svc";
+
+        public User Login(string username, string password)
+        {
+            var myBinding = new BasicHttpBinding();
+            var myEndpoint = new EndpointAddress(loginWebServiceName);
+            ChannelFactory<ILoginService> myChannelFactory = new ChannelFactory<ILoginService>(myBinding, myEndpoint);
+
+            ILoginService client = myChannelFactory.CreateChannel();
+
+            User user = null;
+
+            try
+            {
+                user = client.Login(username, password);
+            }
+            catch (Exception)
+            {
+                if (client != null)
+                {
+                    ((ICommunicationObject)client).Abort();
+                }
+            }
+
+            if (((ICommunicationObject)client).State == CommunicationState.Opened)
+            {
+                ((ICommunicationObject)client).Close();
+            }
+
+            if (user != null && user.Role == Role.WORKER)
+            {
+                return user;
+            }
+
+            return null;
+        }
+
+        public void StartWork(User user, DateTime startTime)
+        {
+            var myBinding = new BasicHttpBinding();
+            var myEndpoint = new EndpointAddress(submitTimeWebServiceName);
+            ChannelFactory<ISubmitTimeService> myChannelFactory = new ChannelFactory<ISubmitTimeService>(myBinding, myEndpoint);
+
+            ISubmitTimeService client = myChannelFactory.CreateChannel();
+
+            try
+            {
+                client.StartWork(user, startTime);
+            }
+            catch (Exception)
+            {
+                if (client != null)
+                {
+                    ((ICommunicationObject)client).Abort();
+                }
+            }
+
+            if (((ICommunicationObject)client).State == CommunicationState.Opened)
+            {
+                ((ICommunicationObject)client).Close();
+            }
+        }
+
+        public void EndWork(User user, DateTime endTime)
+        {
+            var myBinding = new BasicHttpBinding();
+            var myEndpoint = new EndpointAddress(submitTimeWebServiceName);
+            ChannelFactory<ISubmitTimeService> myChannelFactory = new ChannelFactory<ISubmitTimeService>(myBinding, myEndpoint);
+
+            ISubmitTimeService client = myChannelFactory.CreateChannel();
+
+            try
+            {
+                client.EndWork(user, endTime);
+            }
+            catch (Exception)
+            {
+                if (client != null)
+                {
+                    ((ICommunicationObject)client).Abort();
+                }
+            }
+
+            if (((ICommunicationObject)client).State == CommunicationState.Opened)
+            {
+                ((ICommunicationObject)client).Close();
+            }
+        }
+
+        public Dictionary<DateTime, TimeSpan> GetTimes(User user)
+        {
+            var myBinding = new BasicHttpBinding();
+            var myEndpoint = new EndpointAddress(checkTimeWebServiceName);
+            ChannelFactory<ICheckTimeService> myChannelFactory = new ChannelFactory<ICheckTimeService>(myBinding, myEndpoint);
+
+            ICheckTimeService client = myChannelFactory.CreateChannel();
+
+            Dictionary<DateTime, TimeSpan> timesForDays = null;
+
+            try
+            {
+                timesForDays = client.GetTimes(user);
+            }
+            catch (Exception)
+            {
+                if (client != null)
+                {
+                    ((ICommunicationObject)client).Abort();
+                }
+            }
+
+            if (((ICommunicationObject)client).State == CommunicationState.Opened)
+            {
+                ((ICommunicationObject)client).Close();
+            }
+
+            return timesForDays;
+        }
+
+        public Dictionary<DateTime, TimeSpan> GetTimesInInterval(User user, DateTime sinceDate, DateTime untilDate)
+        {
+            var myBinding = new BasicHttpBinding();
+            var myEndpoint = new EndpointAddress(checkTimeWebServiceName);
+            ChannelFactory<ICheckTimeService> myChannelFactory = new ChannelFactory<ICheckTimeService>(myBinding, myEndpoint);
+
+            ICheckTimeService client = myChannelFactory.CreateChannel();
+
+            Dictionary<DateTime, TimeSpan> timesForDays = null;
+
+            try
+            {
+                timesForDays = client.GetTimesInInterval(user, sinceDate, untilDate);
+            }
+            catch (Exception)
+            {
+                if (client != null)
+                {
+                    ((ICommunicationObject)client).Abort();
+                }
+            }
+
+            if (((ICommunicationObject)client).State == CommunicationState.Opened)
+            {
+                ((ICommunicationObject)client).Close();
+            }
+
+            return timesForDays;
+        }
+    }
+}
